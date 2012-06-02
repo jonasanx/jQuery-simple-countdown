780e555cb691cbb568d8672abf9ea851b8249fb8
Jonathan Javier <jonasanx@gmail.com>
2012-06-01 23:06:41 -0500

    Le plugin…

---
+++ jquery.scountdown.js
@@ -0,0 +1,50 @@
+;(function ($) {
+  $.scountdown = function (element, options) {
+    var plugin = this;
+
+    var lastings = {
+      days    : 24*60*60,
+      hours   : 60*60,
+      minutes : 60
+    }
+
+    plugin.init = function () {
+
+      var settings = $.extend({}, {
+        timestamp: 0,
+        callback : function(){}
+      }, options);
+
+      (function countdown(){
+        var timeLeft = Math.floor((settings.timestamp - (new Date())) / 1000);
+        var digits = {}
+
+        if(timeLeft < 0) timeLeft = 0;
+
+        $.each(lastings, function(index, value){
+          digits[index] = add_leading_zero(Math.floor(timeLeft / value));
+          timeLeft -= digits[index] * value;
+        });
+
+        settings.callback(digits.days, digits.hours, digits.minutes, add_leading_zero(timeLeft));
+        setTimeout(countdown, 1000);
+      })();
+
+    }
+
+    // Private methods
+    var add_leading_zero = function (n) {
+      return (n.toString().length < 2) ? '0' + n : n;
+    }
+
+    // Fire!
+    plugin.init();
+  }
+
+  $.fn.scountdown = function (options) {
+    return this.each(function () {
+      var plugin = new $.scountdown(this, options);
+    });
+  }
+
+})(jQuery);
